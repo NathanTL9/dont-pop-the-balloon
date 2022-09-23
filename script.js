9b8c4f0c11e87d098d2ac23691b6aa9c45f2f33d
Glitch (glitch-blank-website) <none>
2022-09-23 21:34:10 +0000

    🌆🐂 Checkpoint ./script.js:48933715/11894 ./index.html:48933715/1226

--- script.js
+++ script.js
@@ -1,44 +1,61 @@
-function handleOrientation(event) {
-  updateFieldIfNotNull('Orientation_a', event.alpha);
-  updateFieldIfNotNull('Orientation_b', event.beta);
-  updateFieldIfNotNull('Orientation_g', event.gamma);
-}
+/*
+Checks to see if button is held and prevents the text selection from popping up
+*/
 
-function updateFieldIfNotNull(fieldName, value, precision=1){
+let button_held = false;
+let inflate_button = document.getElementById("Inflate");
+inflate_button.addEventListener("touchstart", (event) => {
+  button_held = true;
+  event.returnValue = false;
+});
+inflate_button.addEventListener("touchend", (event) => {
+  button_held = false;
+  event.returnValue = false;
+});
+
+let counter = 0;
+function updateFieldIfNotNull(fieldName, value, precision = 1) {
   if (value != null)
     document.getElementById(fieldName).innerHTML = value.toFixed(precision);
 }
 
 function handleMotion(event) {
-  document.getElementById("Inflate").ontouchstart = function() {updateFieldIfNotNull('Accelerometer_z', event.acceleration.z)};
+  if (button_held) {
+    updateFieldIfNotNull("Accelerometer_z", event.acceleration.z);
+  }
+}
+
+function handleOrientation(event) {
+  updateFieldIfNotNull("Orientation_a", event.alpha);
+  updateFieldIfNotNull("Orientation_b", event.beta);
+  updateFieldIfNotNull("Orientation_g", event.gamma);
 }
 
 let is_running = false;
 let demo_button = document.getElementById("StartGame");
-demo_button.onclick = function(e) {
+demo_button.onclick = function (e) {
   e.preventDefault();
-  
-  // Request permission for iOS 13+ devices
+
   if (
     DeviceMotionEvent &&
     typeof DeviceMotionEvent.requestPermission === "function"
   ) {
     DeviceMotionEvent.requestPermission();
   }
-  
-  if (is_running){
+
+  if (is_running) {
     window.removeEventListener("devicemotion", handleMotion);
     window.removeEventListener("deviceorientation", handleOrientation);
     demo_button.innerHTML = "Start demo";
-    demo_button.classList.add('btn-success');
-    demo_button.classList.remove('btn-danger');
+    demo_button.classList.add("btn-success");
+    demo_button.classList.remove("btn-danger");
     is_running = false;
-  }else{
+  } else {
     window.addEventListener("devicemotion", handleMotion);
     window.addEventListener("deviceorientation", handleOrientation);
     document.getElementById("StartGame").innerHTML = "End Game";
-    demo_button.classList.remove('btn-success');
-    demo_button.classList.add('btn-danger');
+    demo_button.classList.remove("btn-success");
+    demo_button.classList.add("btn-danger");
     is_running = true;
   }
 };
