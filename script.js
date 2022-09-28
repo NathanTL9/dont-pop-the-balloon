a697323ab499c2456cf32fd63548db5c5d51d177
Glitch (glitch-blank-website) <none>
2022-09-28 03:48:20 +0000

    🚍⛑ Checkpoint ./script.js:49010746/15676

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ document.addEventListener("touchend", pageReleased);
 document.addEventListener("mousedown", pageClicked);
 document.addEventListener("mouseup", pageReleased);
 
-
 // Setting up canvas
 document.getElementById("canvas").width = window.innerWidth;
 document.getElementById("canvas").height = window.innerHeight;
@@ -26,75 +25,89 @@ let parallax = false;
 let numOfPumps = 0;
 let counter = 0;
 let popped = false;
+let drawBalloon = true;
 let firstClick = true;
-
+let balloonAnim = 0;
 let balloonImage = document.getElementById("balloon");
 
 document.getElementById("begin").onclick = function (e) {
   e.preventDefault();
-      if (typeof DeviceMotionEvent.requestPermission === 'function') {
-      DeviceMotionEvent.requestPermission()
-        .then(permissionState => {
-          if (permissionState === 'granted') {
-            window.addEventListener('devicemotion', handleMotion);
-          }
-        })
-        .catch(console.error);
-    } else {
-      // handle regular non iOS 13+ devices
-    }
+  if (typeof DeviceMotionEvent.requestPermission === "function") {
+    DeviceMotionEvent.requestPermission()
+      .then((permissionState) => {
+        if (permissionState === "granted") {
+          window.addEventListener("devicemotion", handleMotion);
+        }
+      })
+      .catch(console.error);
+  } else {
+    // handle regular non iOS 13+ devices
+  }
 
-   balloonFill = 100;
-   balloonFillVel = 0;
-   houseHeight = 0;
-   houseVel = 0;
-   grassPos = 0;
-   grassVel = 0;
-   mountainPos = 0.1;
-   mountainVel = 0;
-   parallax = false;
-   numOfPumps = 0;
-   counter = 0;
-   popped = false;
-   firstClick = true;
-  
+  balloonFill = 100;
+  balloonFillVel = 0;
+  houseHeight = 0;
+  houseVel = 0;
+  grassPos = 0;
+  grassVel = 0;
+  mountainPos = 0.1;
+  mountainVel = 0;
+  parallax = false;
+  numOfPumps = 0;
+  counter = 0;
+  popped = false;
+  drawBalloon = true;
+  firstClick = true;
+  balloonAnim = 0;
+  balloonImage = document.getElementById("balloon");
+
   numOfPumps = Math.floor(Math.random() * 100) + 25;
   document.getElementById("begin").innerHTML = "Restart";
   window.addEventListener("devicemotion", handleMotion);
 };
 
-
 document.getElementById("end").onclick = function (e) {
-    window.removeEventListener("devicemotion", handleMotion);
-    window.location.href = document.location='index.html';
+  window.removeEventListener("devicemotion", handleMotion);
+  window.location.href = document.location = "index.html";
 };
 
 resizeCanvas();
 window.requestAnimationFrame(gameLoop);
 
 function gameLoop() {
-    tick();
-    render();
-    window.requestAnimationFrame(gameLoop);
+  tick();
+  render();
+  window.requestAnimationFrame(gameLoop);
 }
 
 function tick() {
   if (popped) {
-    
+    if (balloonAnim > 12) {
+      drawBalloon = false;
+    } else if (balloonAnim > 9) {
+      balloonImage = document.getElementById("balloon_pop3");
+    } else if (balloonAnim > 6) {
+      balloonImage = document.getElementById("balloon_pop2");
+    } else if (balloonAnim > 3) {
+      balloonImage = document.getElementById("balloon_pop1");
+    } else if (balloonAnim >= 0) {
+      balloonImage = document.getElementById("balloon_pop0");
+    }
+    balloonAnim++;
     return;
   }
-  
+
   console.log("Fill vel:" + balloonFillVel);
-    balloonFill += balloonFillVel;
-    grassPos += grassVel;
-    mountainPos += mountainVel;
-    houseHeight += houseVel;
-  
+  balloonFill += balloonFillVel;
+  grassPos += grassVel;
+  mountainPos += mountainVel;
+  houseHeight += houseVel;
+
   balloonFillVel -= 0.1;
   grassVel -= 0.1;
   mountainVel -= 0.1;
   houseVel -= 0.1;
-  
+
   if (balloonFillVel < 0) {
     balloonFillVel = 0;
   }
@@ -110,81 +123,160 @@ function tick() {
 }
 
 function render() {
-    let houseSize = window.innerHeight / 3;
-    let balloonSize = (window.innerHeight / 2000) * balloonFill;
-    ctx.fillStyle = '#76d6ff'
-    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-    ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
-    if (parallax) {
-        if (window.innerHeight * (16 / 9) >= window.innerWidth) {
-            ctx.drawImage(document.getElementById("mountains"), -(((window.innerHeight * (16 / 9)) - window.innerWidth) / 2), 0 + mountainPos, window.innerHeight * (16 / 9), window.innerHeight);
-            ctx.drawImage(document.getElementById("grass"), -(((window.innerHeight * (16 / 9)) - window.innerWidth) / 2), 0 + grassPos, window.innerHeight * (16 / 9), window.innerHeight);
-        } else {
-            ctx.drawImage(document.getElementById("mountains"), 0, -(((window.innerWidth * (9 / 16)) - window.innerHeight)) + mountainPos, window.innerWidth, window.innerWidth * (9 / 16));
-            ctx.drawImage(document.getElementById("grass"), 0, -(((window.innerWidth * (9 / 16)) - window.innerHeight)) + grassPos, window.innerWidth, window.innerWidth * (9 / 16));
-        }
+  let houseSize = window.innerHeight / 3;
+  let balloonSize = (window.innerHeight / 2000) * balloonFill;
+  ctx.fillStyle = "#76d6ff";
+  ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+  ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+  if (parallax) {
+    if (window.innerHeight * (16 / 9) >= window.innerWidth) {
+      ctx.drawImage(
+        document.getElementById("mountains"),
+        -((window.innerHeight * (16 / 9) - window.innerWidth) / 2),
+        0 + mountainPos,
+        window.innerHeight * (16 / 9),
+        window.innerHeight
+      );
+      ctx.drawImage(
+        document.getElementById("grass"),
+        -((window.innerHeight * (16 / 9) - window.innerWidth) / 2),
+        0 + grassPos,
+        window.innerHeight * (16 / 9),
+        window.innerHeight
+      );
     } else {
-        if (window.innerHeight * (16 / 9) >= window.innerWidth) {
-            ctx.drawImage(document.getElementById("mountains"), -(((window.innerHeight * (16 / 9)) - window.innerWidth) / 2), 0, window.innerHeight * (16 / 9), window.innerHeight);
-            ctx.drawImage(document.getElementById("grass"), -(((window.innerHeight * (16 / 9)) - window.innerWidth) / 2), 0, window.innerHeight * (16 / 9), window.innerHeight);
-        } else {
-            ctx.drawImage(document.getElementById("mountains"), 0, -(((window.innerWidth * (9 / 16)) - window.innerHeight)), window.innerWidth, window.innerWidth * (9 / 16));
-            ctx.drawImage(document.getElementById("grass"), 0, -(((window.innerWidth * (9 / 16)) - window.innerHeight)), window.innerWidth, window.innerWidth * (9 / 16));
-        }
+      ctx.drawImage(
+        document.getElementById("mountains"),
+        0,
+        -(window.innerWidth * (9 / 16) - window.innerHeight) + mountainPos,
+        window.innerWidth,
+        window.innerWidth * (9 / 16)
+      );
+      ctx.drawImage(
+        document.getElementById("grass"),
+        0,
+        -(window.innerWidth * (9 / 16) - window.innerHeight) + grassPos,
+        window.innerWidth,
+        window.innerWidth * (9 / 16)
+      );
     }
-    if (window.innerHeight - houseSize - houseHeight > window.innerHeight / 2 - houseSize / 2 || parallax) {
-        ctx.drawImage(document.getElementById("house"), (window.innerWidth / 2) - (houseSize / 2), window.innerHeight - houseSize - houseHeight, houseSize, houseSize);
-        ctx.drawImage(document.getElementById("balloon"), (window.innerWidth / 2) - (balloonSize / 2), (window.innerHeight / 2) - (balloonSize) - houseHeight, balloonSize, balloonSize);
+  } else {
+    if (window.innerHeight * (16 / 9) >= window.innerWidth) {
+      ctx.drawImage(
+        document.getElementById("mountains"),
+        -((window.innerHeight * (16 / 9) - window.innerWidth) / 2),
+        0,
+        window.innerHeight * (16 / 9),
+        window.innerHeight
+      );
+      ctx.drawImage(
+        document.getElementById("grass"),
+        -((window.innerHeight * (16 / 9) - window.innerWidth) / 2),
+        0,
+        window.innerHeight * (16 / 9),
+        window.innerHeight
+      );
     } else {
-        ctx.drawImage(document.getElementById("house"), (window.innerWidth / 2) - (houseSize / 2), window.innerHeight / 2 - houseSize / 2, houseSize, houseSize);
-        ctx.drawImage(document.getElementById("balloon"), (window.innerWidth / 2) - (balloonSize / 2), (window.innerHeight / 2) - (balloonSize), balloonSize, balloonSize);
-        parallax = true;
+      ctx.drawImage(
+        document.getElementById("mountains"),
+        0,
+        -(window.innerWidth * (9 / 16) - window.innerHeight),
+        window.innerWidth,
+        window.innerWidth * (9 / 16)
+      );
+      ctx.drawImage(
+        document.getElementById("grass"),
+        0,
+        -(window.innerWidth * (9 / 16) - window.innerHeight),
+        window.innerWidth,
+        window.innerWidth * (9 / 16)
+      );
     }
-
+  }
+  if (
+    window.innerHeight - houseSize - houseHeight >
+      window.innerHeight / 2 - houseSize / 2 ||
+    parallax
+  ) {
+    ctx.drawImage(
+      document.getElementById("house"),
+      window.innerWidth / 2 - houseSize / 2,
+      window.innerHeight - houseSize - houseHeight,
+      houseSize,
+      houseSize
+    );
+    if (drawBalloon) {
+      ctx.drawImage(
+        balloonImage,
+        window.innerWidth / 2 - balloonSize / 2,
+        window.innerHeight / 2 - balloonSize - houseHeight,
+        balloonSize,
+        balloonSize
+      );
+    }
+  } else {
+    ctx.drawImage(
+      document.getElementById("house"),
+      window.innerWidth / 2 - houseSize / 2,
+      window.innerHeight / 2 - houseSize / 2,
+      houseSize,
+      houseSize
+    );
+    if (drawBalloon) {
+      ctx.drawImage(
+        balloonImage,
+        window.innerWidth / 2 - balloonSize / 2,
+        window.innerHeight / 2 - balloonSize,
+        balloonSize,
+        balloonSize
+      );
+    }
+    parallax = true;
+  }
 }
 
 function resizeCanvas() {
-    document.getElementById("canvas").width = window.innerWidth;
-    document.getElementById("canvas").height = window.innerHeight;
-    ctx.imageSmoothingEnabled = false;
+  document.getElementById("canvas").width = window.innerWidth;
+  document.getElementById("canvas").height = window.innerHeight;
+  ctx.imageSmoothingEnabled = false;
 }
 
 function pageClicked() {
-    buttonHeld = true;
-    //console.log("Testing");
+  buttonHeld = true;
+  //console.log("Testing");
 }
 
 function pageReleased() {
-    buttonHeld = false;
-    //console.log("Testing over");
+  buttonHeld = false;
+  //console.log("Testing over");
 }
 
 function trackPumps(value) {
-    //console.log("Motion val:" + value);
-    if (value < -25) {
-        if (counter > numOfPumps) {
-            popped = true;
-            console.log("The balloon popped!");
-        } else {
-            counter++;
-            balloonFillVel = 2.0;
-        
-            if (parallax) {
-                grassVel = 5.0;
-                mountainVel = 4.0;
-                if (houseHeight > window.innerHeight / 20) {
-                    houseVel = -2.0;
-                }
-            } else {
-                houseVel = 5.0;
-            }
+  //console.log("Motion val:" + value);
+  if (value < -25) {
+    if (counter > numOfPumps) {
+      popped = true;
+      console.log("The balloon popped!");
+    } else {
+      counter++;
+      balloonFillVel = 2.0;
+
+      if (parallax) {
+        grassVel = 5.0;
+        mountainVel = 4.0;
+        if (houseHeight > window.innerHeight / 20) {
+          houseVel = -2.0;
         }
+      } else {
+        houseVel = 5.0;
+      }
     }
+  }
 }
 
 function handleMotion(event) {
-    if (buttonHeld) {
-        //console.log("Handling motion");
-        trackPumps(event.acceleration.z);
-    }
+  if (buttonHeld) {
+    //console.log("Handling motion");
+    trackPumps(event.acceleration.z);
+  }
 }
